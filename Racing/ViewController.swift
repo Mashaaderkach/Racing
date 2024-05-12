101c88a49ac54110768aad2b7f24408cd88d792a
Maryia_Dziarkach <mashaaderkach@gmail.com>
2024-05-12 21:01:41 +0300

    XX-3. Changed font

--- Racing/ViewController.swift
+++ Racing/ViewController.swift
@@ -14,6 +14,9 @@ private extension CGFloat {
     static let buttonsHeight: CGFloat = 70
 }
 
+
+
+
 class ViewController: UIViewController {
     
     // MARK: - Properties
@@ -26,28 +29,31 @@ class ViewController: UIViewController {
     
     private let playButton: UIButton = {
         let playButton = UIButton()
-        playButton.setTitle("Play", for: .normal)
-        playButton.setTitleColor(UIColor(red: 176/255, green: 52/255, blue: 22/255, alpha: 1.0), for: .normal)
+        //        playButton.setTitle("Play", for: .normal)
+        //        playButton.setTitleColor(UIColor(red: 176/255, green: 52/255, blue: 22/255, alpha: 1.0), for: .normal)
         playButton.backgroundColor = UIColor(red: 231/255, green: 187/255, blue: 97/255, alpha: 1.0)
-        playButton.titleLabel?.font = UIFont(name: "Chalkboard SE", size: 35)
+        //        playButton.titleLabel?.font = UIFont(name: "Chalkboard SE", size: 35)
+        playButton.layer.cornerRadius = 10
         return playButton
     }()
     
     private let achievmentsButton: UIButton = {
         let achievmentsButton = UIButton()
-        achievmentsButton.setTitle("Achievments", for: .normal)
-        achievmentsButton.setTitleColor(UIColor(red: 176/255, green: 52/255, blue: 22/255, alpha: 1.0), for: .normal)
+        //        achievmentsButton.setTitle("Achievments", for: .normal)
+        //        achievmentsButton.setTitleColor(UIColor(red: 176/255, green: 52/255, blue: 22/255, alpha: 1.0), for: .normal)
         achievmentsButton.backgroundColor = UIColor(red: 231/255, green: 187/255, blue: 97/255, alpha: 1.0)
-        achievmentsButton.titleLabel?.font = UIFont(name: "Chalkboard SE", size: 35)
+        //  achievmentsButton.titleLabel?.font = UIFont(name: "Chalkboard SE", size: 35)
+        achievmentsButton.layer.cornerRadius = 10
         return achievmentsButton
     }()
     
     private let settingsButton: UIButton = {
         let settingsButton = UIButton()
-        settingsButton.setTitle("Settings", for: .normal)
-        settingsButton.setTitleColor(UIColor(red: 176/255, green: 52/255, blue: 22/255, alpha: 1.0), for: .normal)
+        //        settingsButton.setTitle("Settings", for: .normal)
+        //        settingsButton.setTitleColor(UIColor(red: 176/255, green: 52/255, blue: 22/255, alpha: 1.0), for: .normal)
         settingsButton.backgroundColor = UIColor(red: 231/255, green: 187/255, blue: 97/255, alpha: 1.0)
-        settingsButton.titleLabel?.font = UIFont(name: "Chalkboard SE", size: 35)
+        // settingsButton.titleLabel?.font = UIFont(name: "Chalkboard SE", size: 35)
+        settingsButton.layer.cornerRadius = 10
         return settingsButton
     }()
     
@@ -60,6 +66,7 @@ class ViewController: UIViewController {
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        buttonsTextFont()
         configureUI()
     }
     
@@ -93,7 +100,7 @@ class ViewController: UIViewController {
             make.right.equalToSuperview().inset(CGFloat.buttonsRightInset)
             make.height.equalTo(CGFloat.buttonsHeight)
         }
-      
+        
         let playButtonAction = UIAction { _ in
             self.playButtonPressed()
         }
@@ -112,13 +119,58 @@ class ViewController: UIViewController {
     }
     
     
-
+    private func buttonsTextFont() {
+        
+        let shadow = NSShadow()
+        shadow.shadowBlurRadius = 5
+        shadow.shadowOffset = CGSize(width: 1, height: 1)
+        shadow.shadowColor = UIColor.systemBrown
+        
+        
+        let playButtonText = "Play"
+        let playButtonAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor(red: 176/255, green: 52/255, blue: 22/255, alpha: 1.0),
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 35, weight: .semibold),
+            NSAttributedString.Key.shadow: shadow
+        ]
+        let playButtonAttributesText = NSAttributedString(
+            string: playButtonText,
+            attributes: playButtonAttributes
+        )
+        playButton.setAttributedTitle(playButtonAttributesText, for: .normal)
+        
+        
+        let achievmentsButtonText = "Achievments"
+        let achievmentsButtonAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor(red: 176/255, green: 52/255, blue: 22/255, alpha: 1.0),
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 35, weight: .semibold),
+            NSAttributedString.Key.shadow: shadow
+        ]
+        let achievmentsButtonAttributesText = NSAttributedString(
+            string: achievmentsButtonText,
+            attributes: achievmentsButtonAttributes
+        )
+        achievmentsButton.setAttributedTitle(achievmentsButtonAttributesText, for: .normal)
+        
+        
+        let settingsButtonText = "Settings"
+        let settingsButtonAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor(red: 176/255, green: 52/255, blue: 22/255, alpha: 1.0),
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 35, weight: .semibold),
+            NSAttributedString.Key.shadow: shadow
+        ]
+        let settingsButtonAttributesText = NSAttributedString(
+            string: settingsButtonText,
+            attributes: settingsButtonAttributes
+        )
+        settingsButton.setAttributedTitle(settingsButtonAttributesText, for: .normal)
+    }
     
     
     
     
     
-    // MARK: - IBActions
+    
     
     
     
